2b0e22883c4b0ae28cbdde6b721054d61068d24c
Alex Joshua <ialexjoshua@gmail.com>
2022-11-17 21:07:10 +0200

    fix words used

--- App.tsx
+++ App.tsx
@@ -3,6 +3,19 @@ import HangmanDrawing from './components/hangman-drawing/component';
 import WordToGuess from './components/word-to-guess/component';
 import Keyboard from './components/keyboard/component';
 import words from './words.json';
+import { View, StyleSheet, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  gameContainer: {
+    maxWidth: '800px',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '2rem',
+    margin: 'auto',
+    alignItems: 'center',
+  },
+  gameResult: { fontSize: 35, textAlign: 'center' },
+});
 
 function getWord() {
   return words[Math.floor(Math.random() * words.length)].title;
@@ -64,27 +77,22 @@ function App() {
   }, []);
 
   return (
-    <div
-      style={{
-        maxWidth: '800px',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '2rem',
-        margin: '0 auto',
-        alignItems: 'center',
-      }}
-    >
-      <div style={{ fontSize: '2rem', textAlign: 'center' }}>
-        {isWinner && 'Winner!'}
-        {isLoser && 'Nice Try'}
-      </div>
+    <View style={styles.gameContainer}>
+      <View>
+        {isWinner ? (
+          <Text style={styles.gameResult}>{isWinner && 'Winner!'}</Text>
+        ) : null}
+        {isLoser ? (
+          <Text style={styles.gameResult}>{isLoser && 'Nice Try'}</Text>
+        ) : null}
+      </View>
       <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
       <WordToGuess
         reveal={isLoser}
         guessedLetters={guessedLetters}
         wordToGuess={wordToGuess}
       />
-      <div style={{ alignSelf: 'stretch' }}>
+      <View style={{ alignSelf: 'stretch' }}>
         <Keyboard
           disabled={isWinner || isLoser}
           activeLetters={guessedLetters.filter((letter) =>
@@ -93,8 +101,8 @@ function App() {
           inactiveLetters={incorrectLetters}
           addGuessedLetter={addGuessedLetter}
         />
-      </div>
-    </div>
+      </View>
+    </View>
   );
 }
 
