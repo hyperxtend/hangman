c306c08a4a728e9bb2612f24228af47323fbb99a
Alex Joshua <ialexjoshua@gmail.com>
2022-11-17 21:59:38 +0200

    added clues and reset

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import HangmanDrawing from './components/hangman-drawing/component';
 import WordToGuess from './components/word-to-guess/component';
 import Keyboard from './components/keyboard/component';
 import words from './words.json';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
 const styles = StyleSheet.create({
   gameContainer: {
@@ -14,16 +14,33 @@ const styles = StyleSheet.create({
     margin: 'auto',
     alignItems: 'center',
   },
+  clueContainer: { flexDirection: 'row', gap: 50 },
   gameResult: { fontSize: 35, textAlign: 'center' },
+  clue: {
+    fontSize: 14,
+    textTransform: 'uppercase',
+    fontWeight: '600',
+  },
 });
 
-function getWord() {
-  return words[Math.floor(Math.random() * words.length)].title;
+function getSong() {
+  return words[Math.floor(Math.random() * words.length)];
 }
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState(getWord);
+  const [songToGuess, setSongToGuess] = useState(getSong);
+  const songTitle = songToGuess.title;
+
+  const songArtist = songToGuess.artist;
+  const songAlbum = songToGuess.album;
+  const songYear = songToGuess.year;
+
+  const [wordToGuess] = useState(songTitle);
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  const [showSongArtist, setShowSongArtist] = useState<boolean>(false);
+  const [showSongAlbum, setShowSongAlbum] = useState<boolean>(false);
+  const [showSongYear, setShowSongYear] = useState<boolean>(false);
 
   const incorrectLetters = guessedLetters.filter(
     (letter) => !wordToGuess.includes(letter)
@@ -66,7 +83,7 @@ function App() {
 
       e.preventDefault();
       setGuessedLetters([]);
-      setWordToGuess(getWord());
+      setSongToGuess(getSong());
     };
 
     document.addEventListener('keypress', handler);
@@ -75,9 +92,49 @@ function App() {
       document.removeEventListener('keypress', handler);
     };
   }, []);
-
+  function resetGame() {
+    setSongToGuess(getSong());
+    setGuessedLetters(wordToGuess.split());
+    setShowSongArtist(false);
+    setShowSongAlbum(false);
+    setShowSongYear(false);
+  }
   return (
     <View style={styles.gameContainer}>
+      <View style={styles.clueContainer}>
+        <View>
+          <Button title="REST" onPress={() => resetGame()} />
+        </View>
+        <View>
+          <Button
+            title="show artist"
+            onPress={() => {
+              setShowSongArtist(true);
+            }}
+          />
+          {showSongArtist ? (
+            <Text style={styles.clue}>{songArtist}</Text>
+          ) : null}
+        </View>
+        <View>
+          <Button
+            title="show album"
+            onPress={() => {
+              setShowSongAlbum(true);
+            }}
+          />
+          {showSongAlbum ? <Text style={styles.clue}>{songAlbum}</Text> : null}
+        </View>
+        <View>
+          <Button
+            title="show artist"
+            onPress={() => {
+              setShowSongYear(true);
+            }}
+          />
+          {showSongYear ? <Text style={styles.clue}>{songYear}</Text> : null}
+        </View>
+      </View>
       <View>
         {isWinner ? (
           <Text style={styles.gameResult}>{isWinner && 'Winner!'}</Text>
